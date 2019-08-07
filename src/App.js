edd01646587ef5b8daaae46bfb5a2b7155af172d
Stefany Cárdenas Martínez <st.cardenas@udla.edu.co>
2019-08-07 18:52:21 -0500

    Cambio Stefany add h1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          <hi>Cambio de Sebastian</hi>
+          <hi>Cambio de Stefany</hi>
+          <hi>Cambio de Stefany</hi>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
